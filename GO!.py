8463003231c4e81c6cdd527c01453461238827e3
Kutsal-Cay <61744655+Kutsal-Cay@users.noreply.github.com>
2024-01-01 10:56:14 +0300

    Add files via upload

--- GO!.py
+++ GO!.py
@@ -4,38 +4,41 @@ import time
 
 #değişkenler
 
+
+
 #mesaj kutusu koordinatı
-msg_coordinate = (416, 817)
+msg_coordinate = (256, 947)
 #en alttaki kullanıcının koordinatı
-user_coordinate = (182, 741)
+user_coordinate = (500, 986)
 
-
+time.sleep(5)
 #sonsuz döngü
 while(True):
+    
     #mouse u en alttaki kullanıcıya getir(1.mönitörü baz alarak koordinatlar girilir)
     pyautogui.moveTo(user_coordinate)
-    time.sleep(0.1)
+    time.sleep(0.2)
     #tıkla
     pyautogui.click()
-    time.sleep(0.1)
+    
+    time.sleep(0.2)
     #mesaj kutusuna tıkla
     pyautogui.moveTo(msg_coordinate)
-    pyautogui.click
-    time.sleep(0.1)
+    pyautogui.click()
     time.sleep(0.1)
     #mesajı yaz
-    pyautogui.typewrite("Happy new year bro")
+    pyautogui.typewrite("Happy new year bro!!!!")
     time.sleep(0.1)
     #enter a bas
     pyautogui.press('enter')
     time.sleep(0.1)
-    #en aşağı in
-    pyautogui.scroll(-9999999)
-    # mouse u en alttaki kullanıcıya getir
+    #mouse u aşağı inmek için barın konumuna al
     pyautogui.moveTo(user_coordinate)
     time.sleep(0.1)
-    # 1 kullanıcı aşağı tekerleği kullanarak in
-    pyautogui.scroll(-80)
+    #en aşağı in
+    pyautogui.scroll(-9999999)
     time.sleep(0.1)
+   
+
 
 
